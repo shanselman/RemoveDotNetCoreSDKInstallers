2cb70416e12e05ec58d91255091898e5f0c370a2
Christopher Lewis <clewis@3CloudSolutions.com>
2018-10-01 09:25:11 -0500

    Added Remove all but latest, silent uninstall minus elevation prompt

--- RemoveCoreSDKs.ps1
+++ RemoveCoreSDKs.ps1
@@ -2,10 +2,18 @@ $app = Get-WmiObject -Class Win32_Product | Where-Object {
     $_.Name -match "Microsoft .NET Core SDK" 
 }
 
-Write-Host $app.Name 
-Write-Host $app.IdentifyingNumber
-pushd $env:SYSTEMROOT\System32
+#Keep latest
+$app | Sort-Object { [System.Version]::new($_.Version)} -Descending | 
+    Select-Object -First  1 | ForEach-Object { 
+        Write-Host ("Keeping " + $_.Name + " version " + $_.Version)
+    }
 
-$app.identifyingnumber |% { Start-Process msiexec -wait -ArgumentList "/x $_" }
-
-popd
+#Convert WMI version to System.version, sort decending, skip latest 
+$app | Sort-Object { [System.Version]::new($_.Version)} -Descending | 
+    Select-Object -Skip 1 | 
+        ForEach-Object { 
+            Write-Host ("*** Removing  " + $_.Name + " version " + $_.Version + " IdentifyingNumber: " + $_.IdentifyingNumber)
+            pushd $env:SYSTEMROOT\System32
+            Start-Process msiexec -wait -ArgumentList ("/x " + $_.identifyingnumber + " /qn IGNOREDEPENDENCIES=ALL")
+            popd
+        }
