f25f3dab3d8668262f5069c437b5c5aaf0fe0c63
Ashley Bayes <85616681+katelynnashley95icloudcom@users.noreply.github.com>
2021-07-07 09:38:07 -0400

    Uodate Removecoresdks.md

--- RemoveCoreSDKs.ps1
+++ RemoveCoreSDKs.ps1
@@ -1,11 +1,6 @@
 $app = Get-WmiObject -Class Win32_Product | Where-Object { 
     $_.Name -match "Microsoft .NET Core SDK" 
 }
-
 Write-Host $app.Name 
 Write-Host $app.IdentifyingNumber
-pushd $env:SYSTEMROOT\System32
-
-$app.identifyingnumber |% { Start-Process msiexec -wait -ArgumentList "/x $_" }
-
-popd
+pushd $env:SYSTEMROOT\System1
